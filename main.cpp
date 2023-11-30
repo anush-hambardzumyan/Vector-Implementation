0dc542db58e3f1a3d8c0caedcbfbd9b7724192f6
anush-hambardzumyan <foreveranham@gmail.com>
2023-11-30 22:39:18 +0400

    added operators for bitvec

--- main.cpp
+++ main.cpp
@@ -2,27 +2,41 @@
 
 int main()
 {
-    // MyVector<bool> vec;
-    // vec.push_back(1);
-    // vec.push_back(0);
-    // vec.push_back(0);
-    // vec.push_back(0);
-    // vec.push_back(0);
-    // vec.push_back(1);
+    MyVector<bool> vec;
+    vec.push_back(1);
+    vec.push_back(0);
+    vec.push_back(0);
+    vec.push_back(0);
+    vec.push_back(1);
+    vec.push_back(1);
+    std::cout << vec;
     // vec.print();
-    // vec.insert(2,1);
-    // //vec.push_back(1);
+    // for(size_t i = 0; i < vec.size() ; ++i)
+    // {
+    //     std::cout << vec[i];
+    // }
+    // std::cout << std::endl;
+    // vec[2] = 1;
+    // vec.print();
+
+    // MyVector<bool> vec1  = std::move(vec);
+    // vec1.print();
+    // vec.print();
+
+
+    // MyVector<bool> vec2 {1,0,1,0,1,1,1,0};
+    // vec2.print();
+
+    // MyVector<bool> vec3(vec1);
+    // vec3.print();
+
+    //vec.insert(2,1);
+    //vec.push_back(1);
     // vec.print();
     // vec.pop_back();
     // vec.print();
     // std::cout << vec.size();
 
-    MyVector<int> vec1(2,10);
-    MyVector<int> vec2 (4,7);
-
-    std::cout << vec1;
-    std::cout << vec2;
-    vec1.swap(vec2);
-    std::cout << vec1;
-    std::cout << vec2;
+    // MyVector<int> vec1(2,10);
+    // MyVector<int> vec2 (4,7);
 }
